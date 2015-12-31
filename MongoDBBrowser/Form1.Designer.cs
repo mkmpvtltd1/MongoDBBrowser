7f5072aa0b02233207ba9514ec74582815d6d371
Manoj Mahato <mmahato@braindigit.com>
2015-12-31 12:13:32 +0545

    Connect To server Test

--- MongoDBBrowser/Form1.Designer.cs
+++ MongoDBBrowser/Form1.Designer.cs
@@ -30,30 +30,116 @@
         {
             this.Status = new System.Windows.Forms.StatusStrip();
             this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Status
             // 
             this.Status.Location = new System.Drawing.Point(0, 363);
             this.Status.Name = "Status";
-            this.Status.Size = new System.Drawing.Size(690, 22);
+            this.Status.Size = new System.Drawing.Size(540, 22);
             this.Status.TabIndex = 1;
             this.Status.Text = "statusStrip1";
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(12, 12);
+            this.btnConnect.Location = new System.Drawing.Point(434, 53);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(139, 23);
+            this.btnConnect.Size = new System.Drawing.Size(100, 23);
             this.btnConnect.TabIndex = 2;
             this.btnConnect.Text = "Connect Server";
             this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(7, 27);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(197, 20);
+            this.txtServer.TabIndex = 5;
+            this.txtServer.Text = "ds037215.mongolab.com";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(215, 27);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.TabIndex = 6;
+            this.txtPort.Text = "37215";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(434, 27);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 10;
+            this.txtPassword.Text = "Com456";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(322, 27);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(106, 20);
+            this.txtUser.TabIndex = 9;
+            this.txtUser.Text = "manoj";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(431, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(319, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "User";
             // 
             // MongoDBBrowser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(690, 385);
+            this.ClientSize = new System.Drawing.Size(540, 385);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.Status);
             this.Name = "MongoDBBrowser";
@@ -67,6 +153,14 @@
 
         private System.Windows.Forms.StatusStrip Status;
         private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- MongoDBBrowser/Form1.cs
+++ MongoDBBrowser/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace MongoDBBrowser
 {
     public partial class MongoDBBrowser : Form
     {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
         public MongoDBBrowser()
         {
             InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ConnectServer();
+        }
+
+        private void ConnectServer()
+        {
+            string ConnectionString = "mongodb://Manoj:Com456@ds037215.mongolab.com:37215/mkmpvtltd";
+           // setting.Server.Port = txtPort.Text;
+            _client = new MongoClient(ConnectionString);
+
+            _database = _client.GetDatabase("mkmpvtltd");
         }
     }
 }
